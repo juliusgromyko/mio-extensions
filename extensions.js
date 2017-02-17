c94289dd0f9dc0ebeab2cfe6eef03be6fe8aa815
IULIY GROMYKO <juliusgromyko@gmail.com>
2017-02-17 17:52:06 +0200

    Added additional header check and support for text response

--- extensions.js
+++ extensions.js
@@ -12,7 +12,9 @@ function extract(req, res, next){
   var urlExtension = req.query.format;
   var headerExtensions = req.headers.format;
   var acceptsExtensions = req.accepts(['html', 'json', 'xml']);
-  var extension = pathExtension || urlExtension || headerExtensions || acceptsExtensions;
+  var contentType = req.headers['content-type'].split('/');
+  contentType = contentType[contentType.length-1];
+  var extension = pathExtension || urlExtension || contentType || headerExtensions || acceptsExtensions;
 
   req.extension = extension;
   next();
@@ -20,16 +22,12 @@ function extract(req, res, next){
 
 // Response render
 function render(req, res, options){
-  console.log("!");
-  
   // Return 501 error for unknown path
   if(!options){
     res.status(501);
     res.send('501 Not Implemented');
     return;
   }
-
-  console.log(options);
 
   // Set status code
   if(options.status){
@@ -67,8 +65,14 @@ function render(req, res, options){
   // Send error response
   default:
     res.type('text');
-    res.status(406);
-    res.send('406 Not Acceptable');
+    if (typeof options.data == 'string' || options.data instanceof String){
+      res.send(options.data);
+    }else if(typeof options.data.message == 'string' || options.data.message instanceof String){
+      res.send(options.data.message);
+    }else{
+      res.status(406);
+      res.send('406 Not Acceptable');
+    }
   }
 }
 
